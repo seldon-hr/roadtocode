798aad4060dd0e5af8c1d925061a50ba58903490
Daniel Romero <daanmaro99june@gmail.com>
2021-05-17 18:14:29 -0500

    Conditions on js

---
+++ js/if.js
@@ -0,0 +1,18 @@
+var edad = 18;
+
+
+if(edad === 18){
+    console.log("You can vote, congrats is your first vote");
+}else if(edad > 18){
+    console.log("You can vote again");
+}else{
+    console.log("You can't vote yet")
+}
+
+//Condicionarodr ternario
+
+condition ? true: false;
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Sí soy un uno" : "No, no soy un uno";
